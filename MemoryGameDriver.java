64ddb9a27ff7b5828f873ad62086c7857d6827a8
mackinnonm <49697668+mackinnonm@users.noreply.github.com>
2019-04-22 20:29:32 -0400

    Add files via upload

--- MemoryGameDriver.java
+++ MemoryGameDriver.java
@@ -3,7 +3,14 @@ import java.util.*;
 class MemoryGameDriver{
    public static void main(String argv[]){
       Scanner sc = new Scanner(System.in); 
-   
-      MemoryGame mg = new MemoryGame(); 
+      int takeTurn=0;
+        
+      MemoryGame mg = new MemoryGame();
+       mg.setUp();
+       mg.createBackBoard();
+       mg.createImageBoard();
+
+       System.out.println("In this game you need to match");
+       System.out.println("the button pairs in order to win."); 
    } // main
 } // class
